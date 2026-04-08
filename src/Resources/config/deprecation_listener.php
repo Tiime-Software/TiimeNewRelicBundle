98bebf25f46648e68436883be7a770ab030591ab
rflavien <flavien.rodrigues@tiime.fr>
2026-04-08 10:58:26 +0200

    cs

--- src/Resources/config/deprecation_listener.php
+++ src/Resources/config/deprecation_listener.php
@@ -2,6 +2,13 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Tiime New Relic bundle.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Tiime\NewRelicBundle\Listener\DeprecationListener;
 
